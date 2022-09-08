14e04ed92672730e1fdb9c8b524b35bc92710ff4
Gabriel Patara <gabriel.patara@gmail.com>
2022-09-08 06:56:02 -0300

    tentando arrumar o auto click

--- js/app.js
+++ js/app.js
@@ -1,21 +1,16 @@
 const dollar = document.getElementById('dollar');
 const tela = document.getElementById('tela');
 const click = document.getElementById('click');
-
-//10
-const dez = document.getElementById('10');
-const teladez = document.getElementById('tela10');
-const telaprecodez = document.getElementById('precodez');
-let precodez = 10;
-
-tela.textContent = '$0'
-let carteira= 0;
-
 let n = 1;
+let carteira= 0;
+tela.textContent = '$0';
 
-//investimentos
-let numDez = 0;
 
+
+
+
+
+//Funções 
 
 function reset(){
     dollar.style.width = '30vw';
@@ -30,17 +25,65 @@ function clicker(){
     setTimeout(() => {click.style.bottom = '130px';click.style.opacity = '0%'}, 300);
 }
 
+function autoClick(numAuto){
+
+    setTimeout(()=>{
+        
+        click.textContent = "+"+10;
+        clicker();
+        carteira+=10;
+        tela.textContent = "$"+carteira;
+        dollar.style.width = '300px';
+        setTimeout(reset,40);
+    },1500);
+    setTimeout(()=>{autoClick(numAuto);},1500)
+}
+
 
 
 dollar.onclick = () =>{
 
-    dollar.style.width = '25vw';
+    dollar.style.width = '300px';
     setTimeout(reset,40);
     carteira += n;
     tela.textContent = '$'+carteira;
     click.textContent = "+"+n;
     clicker();
 }
+
+//funcionalidades das compras
+
+//Click Automatico -----------------------------------------------------
+
+const auto = document.getElementById('auto');
+const telaauto = document.getElementById('telaauto');
+const telaprecoauto = document.getElementById('precoauto');
+const telaTempoClick = document.getElementById('tempoClick');
+let precoAuto = 10;
+let numAuto = 0;
+
+auto.onclick = () =>{
+   if(carteira>=precoAuto){
+        carteira-=precoAuto;
+        numAuto += 1;
+        precoAuto = precoAuto*8;
+        if(numAuto>=1){autoClick(numAuto)}
+        telaTempoClick.textContent = numAuto+" cliques a cada 1 segundo";
+        telaprecoauto.textContent = "$"+precoAuto
+        telaauto.textContent = numAuto;
+        tela.textContent = "$"+carteira;
+   }else{
+        alert('Você não possui dinheiro para isso no momento.');
+   }
+};
+
+//Primeiros investimentos -------------------------------------------------
+
+const dez = document.getElementById('10');
+const teladez = document.getElementById('tela10');
+const telaprecodez = document.getElementById('precodez');
+let precodez = 10;
+let numDez = 0;
 
 dez.onclick = () =>{
     
@@ -49,12 +92,13 @@ dez.onclick = () =>{
         precodez = precodez*3;
         numDez += 1;
         n += 1
+
         telaprecodez.textContent = '$'+precodez;
         tela.textContent = '$'+carteira;
         teladez.textContent = numDez;
 
     }else{
-        alert('Você não possui dinheiro para isso no momento.')
+        alert('Você não possui dinheiro para isso no momento.');
     }
 }
 
